b15a63bddd0ae7135b7c1e24e9290b6c27a1e962
Prasanta Mukherjee <04933E744@192.168.0.10>
2022-03-30 15:42:43 +0530

    Test

---
+++ src/AddNumbers/AddNums.java
@@ -0,0 +1,18 @@
+package AddNumbers;
+
+public class AddNums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 {
+				int a=10;
+				int b=20;
+				int c=30;
+				int sum = a + b + c;
+				
+		        System.out.println(sum);
+		    }
+
+	}
+
+}
